8c42f803cc350c1568985e7d7e2aaede4417e0b0
Lex Sapkota <lexsapkota@gmail.com>
2018-03-08 22:14:26 +1100

    XcelToken: Test for contract init parameters.

--- test/TestXcelToken.js
+++ test/TestXcelToken.js
@@ -2,62 +2,53 @@
 const XcelToken = artifacts.require('../contracts/XcelToken.sol')
 
 contract('XcelToken', accounts => {
-  const owner = accounts[0];
-  const tokenBuyerWallet = accounts[2];
+  const tokenBuyerWallet = accounts[2]
   // const teamVestingAddress = accounts[3];
   // const teamVestingContractAddress = 0x123;
 
-  describe('constructor', () => {
-
-    it('returns the expected public variables', async () => {
-      let token = new XcelToken(tokenBuyerWallet)
-      const tokenName = await token.name()
-      const tokenSymbol = await token.symbol()
-      const tokenDecimals = await token.decimals()
-      const tokenSupply = await token.initialSupply()
-
-      assert.equal(tokenName, 'XCELTOKEN')
-      assert.equal(tokenSymbol, 'XCEL')
-      assert.equal(tokenDecimals.toNumber(), 18)
-      assert.equal(tokenSupply.toNumber(), 50 * (10 ** 9) * (10 ** uint256(18)))
+  it('should setup XcelToken with correct init parameters', function() {
+    var xcelToken;
+    return XcelToken.new(tokenBuyerWallet).then(function(instance) {
+      xcelToken = instance;
+      xcelToken.name().then(function(name){
+        assert.equal(name, "XCELTOKEN", "token name should be XCELTOKEN");
+      });
+      xcelToken.symbol().then(function(symbol){
+        assert.equal(symbol, "XCEL", "token symbol should be XCEL");
+      });
+      xcelToken.decimals().then(function(decimals){
+        assert.equal(decimals, 18, "token decimal places should be 18");
+      });
+      xcelToken.INITIAL_SUPPLY().then(function(supply){
+        assert.equal(supply, 50 * (10 ** 9) * (10 ** 18), "token initial supply should be 50 billion");
+      });
+      xcelToken.foundationSupply().then(function(foundationSupply){
+        assert.equal(foundationSupply, 5 * (10 ** 9) * (10 ** 18), "foundation supply should be 10%");
+      });
+      xcelToken.teamSupply().then(function(teamSupply){
+        assert.equal(teamSupply, 7.5 * (10 ** 9) * (10 ** 18), "team supply should be 15%");
+      });
+      xcelToken.publicSaleSupply().then(function(publicSaleSupply){
+        assert.equal(publicSaleSupply, 30 * (10 ** 9) * (10 ** 18), "public sale supply should be 60%");
+      });
+      xcelToken.loyaltySupply().then(function(loyaltySupply){
+        assert.equal(loyaltySupply, 2.5 * (10 ** 9) * (10 ** 18), "loyalty supply should be 5%");
+      });
+      xcelToken.reserveFundSupply().then(function(reserveFundSupply){
+        assert.equal(reserveFundSupply, 5 * (10 ** 9) * (10 ** 18), "reserve supply should be 10%");
+      });
     })
+  });
 
-    it('returns the totalSupply', async () => {
-      let token = new XcelToken(tokenBuyerWallet)
-      const initialSupply = await token.initialSupply()
-      const totalSupply = await token.totalSupply()
-
-      assert.equal(totalSupply.toNumber(), initialSupply.toNumber())
-      assert.equal(totalSupply.toNumber(), tkrToTkrSmall(65500000))
-    })
-
-    it('sets the correct owner of the contract', async () => {
-      let token = new XcelToken(tokenBuyerWallet)
-      assert.equal(await token.owner(), owner)
-    })
-
-    it('stores the entire initial balance in the owner account', async () => {
-      let token = XcelToken.new()
-      const ownerBalance = await token.balanceOf(owner)
-      assert.equal(ownerBalance.toNumber(), tkrToTkrSmall(65500000))
-    })
-
-  })
-
-  describe('Pause / Unpause', () => {
-
-    it("should deploy XcelToken with paused as false", function() {
-        var xcelToken = new XcelToken(tokenBuyerWallet);
-
-        xcelToken.paused().then(function(isPaused){
-          assert.equal(isPaused, false, "paused should be initialized to false");
-        });
-
+  it("should deploy XcelToken with paused as false", function() {
+    var xcelToken = new XcelToken(tokenBuyerWallet);
+    xcelToken.paused().then(function(isPaused){
+      assert.equal(isPaused, false, "paused should be initialized to false");
     });
+   });
 
-    it("should deploy XcelToken paused as false, then true, then false again", function() {
+   it("should deploy XcelToken paused as false, then true, then false again", function() {
       var xcelToken;
-
       return XcelToken.new(tokenBuyerWallet).then(function(instance) {
         xcelToken = instance;
         return xcelToken.paused();
@@ -73,6 +64,4 @@ contract('XcelToken', accounts => {
         assert.equal(isPaused,false,"paused should be set back to false");
       });
     });
-  })
-
-});
+  });
