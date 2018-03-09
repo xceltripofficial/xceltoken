a460d23283d684b297305a221132223dee5eaac3
Kumar Pandey <kumar.pandey@gmail.com>
2018-03-09 12:33:29 -0800

    cleaned up syntanx for promise->then , BurnalbeToken burn test

--- test/TestXcelToken.js
+++ test/TestXcelToken.js
@@ -8,33 +8,33 @@ contract('XcelToken', accounts => {
 
   it('should setup XcelToken with correct init parameters', function() {
     var xcelToken;
-    return XcelToken.new(tokenBuyerWallet).then(function(instance) {
+    return XcelToken.new(tokenBuyerWallet).then(instance => {
       xcelToken = instance;
-      xcelToken.name().then(function(name){
+      xcelToken.name().then(name =>{
         assert.equal(name, "XCELTOKEN", "token name should be XCELTOKEN");
       });
-      xcelToken.symbol().then(function(symbol){
+      xcelToken.symbol().then(symbol=>{
         assert.equal(symbol, "XCEL", "token symbol should be XCEL");
       });
-      xcelToken.decimals().then(function(decimals){
+      xcelToken.decimals().then( decimals =>{
         assert.equal(decimals, 18, "token decimal places should be 18");
       });
-      xcelToken.INITIAL_SUPPLY().then(function(supply){
+      xcelToken.INITIAL_SUPPLY().then( supply =>{
         assert.equal(supply, 50 * (10 ** 9) * (10 ** 18), "token initial supply should be 50 billion");
       });
-      xcelToken.foundationSupply().then(function(foundationSupply){
+      xcelToken.foundationSupply().then(foundationSupply =>{
         assert.equal(foundationSupply, 5 * (10 ** 9) * (10 ** 18), "foundation supply should be 10%");
       });
-      xcelToken.teamSupply().then(function(teamSupply){
+      xcelToken.teamSupply().then(teamSupply =>{
         assert.equal(teamSupply, 7.5 * (10 ** 9) * (10 ** 18), "team supply should be 15%");
       });
-      xcelToken.publicSaleSupply().then(function(publicSaleSupply){
+      xcelToken.publicSaleSupply().then(publicSaleSupply =>{
         assert.equal(publicSaleSupply, 30 * (10 ** 9) * (10 ** 18), "public sale supply should be 60%");
       });
-      xcelToken.loyaltySupply().then(function(loyaltySupply){
+      xcelToken.loyaltySupply().then( loyaltySupply =>{
         assert.equal(loyaltySupply, 2.5 * (10 ** 9) * (10 ** 18), "loyalty supply should be 5%");
       });
-      xcelToken.reserveFundSupply().then(function(reserveFundSupply){
+      xcelToken.reserveFundSupply().then( reserveFundSupply =>{
         assert.equal(reserveFundSupply, 5 * (10 ** 9) * (10 ** 18), "reserve supply should be 10%");
       });
     })
@@ -42,25 +42,27 @@ contract('XcelToken', accounts => {
 
   it("should deploy XcelToken with paused as false", function() {
     var xcelToken = new XcelToken(tokenBuyerWallet);
-    xcelToken.paused().then(function(isPaused){
+    xcelToken.paused().then( isPaused =>{
       assert.equal(isPaused, false, "paused should be initialized to false");
     });
    });
 
    it("should deploy XcelToken paused as false, then true, then false again", function() {
       var xcelToken;
-      return XcelToken.new(tokenBuyerWallet).then(function(instance) {
+      return XcelToken.new(tokenBuyerWallet).then( instance => {
         xcelToken = instance;
         return xcelToken.paused();
-      }).then(function(isPaused) {
+    }).then( isPaused => {
         assert.equal(isPaused, false, "paused should be initialized to false");
-        xcelToken.pause();
+        return xcelToken.pause();
+    }).then (result => {
         return xcelToken.paused();
-      }).then(function(isPaused) {
+    }).then( isPaused => {
         assert.equal(isPaused, true, "paused should be set to true");
-        xcelToken.unpause();
+        return xcelToken.unpause();
+    }).then( result => {
         return xcelToken.paused();
-      }).then(function(isPaused) {
+    }).then( isPaused => {
         assert.equal(isPaused,false,"paused should be set back to false");
       });
     });
