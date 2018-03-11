714cc025757c3b99bed11139d9c099d364209c86
Kumar Pandey <kumar.pandey@gmail.com>
2018-03-11 08:45:34 -0700

    added solium linter updated with results of the linter

--- contracts/MintableTestPlaceholder.sol
+++ contracts/MintableTestPlaceholder.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.4.19;
   This file is being included here eventhough its only used in the Testing Stepvesting.
   Otherwise test is not able to pick it up.
 */
-import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 contract MintableTestPlaceholder{}
